efcad7ad9504ccdf4b5c416261571a8ba16857fa
Solomon Hykes <solomon@dagger.io>
2024-08-14 17:39:46 -0700

    ci: generate (some) github actions pipelines
    This uses a Dagger pipeline to generate the Github Actions pipelines to run our Dagger pipelines.
    
    To generate, run this command from the root of the repo:
    
      dagger call -m .github config -o .github
    
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ .github/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/cubzh/cubzh/.github/internal/dagger"
+)
+
+type Github struct{}
+
+func (m *Github) Config() *dagger.Directory {
+	ci := dag.
+		Gha(dagger.GhaOpts{
+			PublicToken: "p.eyJ1IjogIjFiZjEwMmRjLWYyZmQtNDVhNi1iNzM1LTgxNzI1NGFkZDU2ZiIsICJpZCI6ICI4ZmZmNmZkMi05MDhiLTQ4YTEtOGQ2Zi1iZWEyNGRkNzk4MTkifQ.l1Sf1gB37veXUWhxOgmjvjYcrh32NiuovbMxvjVI7Z0",
+		}).
+		WithPipeline(
+			"Lua Modules (linter)",
+			"lint-modules --src=.:modules",
+			dagger.GhaWithPipelineOpts{
+				Dispatch: true,
+				SparseCheckout: []string{
+					"lua",
+				},
+			},
+		).
+		WithPipeline(
+			"Core Unit Tests",
+			"test-core --src=.:test-core",
+			dagger.GhaWithPipelineOpts{
+				Dispatch: true,
+				Lfs:      true,
+				SparseCheckout: []string{
+					"core",
+					"deps/libz",
+				},
+			}).
+		WithPipeline(
+			"Core clang-format",
+			"lint-core --src=.:lint-core",
+			dagger.GhaWithPipelineOpts{
+				Dispatch: true,
+				Lfs:      true,
+				SparseCheckout: []string{
+					"core",
+					"deps/libz",
+				},
+			})
+	return ci.
+		OnPullRequest(
+			[]string{
+				"Lua Modules (linter)",
+				"Core Unit Tests",
+				"Core clang-format",
+			},
+			dagger.GhaOnPullRequestOpts{
+				Branches: []string{"main"},
+			}).
+		Config().
+		Directory(".github")
+}
